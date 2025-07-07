57d20b11a5e3ae75601c0b7dfba97aadec0e05ab
Aditya <waghaditya312@gmail.com>
2025-07-07 13:42:43 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles/globals.css'
 
+// ⭐ Add this import
+import { ToastProvider } from '@radix-ui/react-toast'  // or wherever your ToastProvider is
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* ⭐ Wrap App inside ToastProvider */}
+    <ToastProvider>
+      <App />
+    </ToastProvider>
   </React.StrictMode>,
 )
